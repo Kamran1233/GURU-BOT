aeb78a3229411fb75fe11ad3cb924824d60d9f41
Kamran1233 <157046453+Kamran1233@users.noreply.github.com>
2024-01-19 08:53:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923121662791', 'kamran1233', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
